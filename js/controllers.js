c7de0ab32648691072c7091e4cb4afaa0e335e69
Hack Reactor Students <github@hackreactor.com>
2013-09-04 17:31:55 -0700

    implemented post info & votes

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,26 @@
-angular.module('myApp.controllers', []);
+angular.module('myApp.controllers', [])
+.controller('MainController', ['$scope', '$http', function($scope, $http){
+  $http({
+    method: 'JSONP',
+    url: 'http://api.reddit.com/r/funny.json?jsonp=JSON_CALLBACK'
+  }).success(function(data){
+    $scope.redditData = data.data.children;
+  }).error(function(data, status){
+    console.log('ERROR!');
+  });
+  $scope.votes = {};
+  $scope.upvote = function(index){
+    $scope.votes[index] = $scope.votes[index] + 1 || 1;
+  };
+  $scope.downvote = function(index){
+    $scope.votes[index] = $scope.votes[index] - 1 || -1;
+  };
+  $scope.displayVote = function(index){
+    return $scope.votes[index] || 0;
+  };
+  $scope.createVoteCounter = function(id){
+    //console.log(id);
+    $scope.votes[id] = 0;
+    //return $scope.votes[id];
+  };
+}]);
--- scrap/js/main.js
+++ scrap/js/main.js
@@ -7,6 +7,13 @@ app.controller('MyController', function($scope) {
   $scope.person = {
     name: 'bo peep'
   };
+  var updateClock = function() {
+    $scope.clock = new Date();
+  };
+  var timer = setInterval(function() {
+    $scope.$apply(updateClock);
+  }, 1000);
+  updateClock();
 });
 
 app.controller('PlayerController', ['$scope', function($scope){
@@ -30,4 +37,24 @@ app.controller('PlayerController', ['$scope', function($scope){
 
 app.controller('RelatedController', ['$scope', function($scope){
 
+}]);
+
+app.controller('DemoController', function($scope){
+  $scope.counter = 0;
+  $scope.add = function(amount) {$scope.counter += amount;};
+  $scope.subtract = function(amount) {$scope.counter-= amount;};
+});
+
+app.controller('httpController', ['$scope', '$http', function($scope, $http){
+  $http({
+    method: 'JSONP',
+    url: 'http://api.openbeerdatabase.com/v1/beers.json?callback=JSON_CALLBACK'
+  }).success(function(data, status, headers, config) {
+    $scope.beerdata = data;
+    // data contains the response
+    // status is the HTTP status
+    // headers is the header getter function
+    // config is the object that was used to create the HTTP request
+  }).error(function(data, status, headers, config) {
+  });
 }]);
